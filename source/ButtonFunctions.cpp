13aae7d5e097e0168de0b5b44f48a41f58d0cc9f
jocelynjiang1 <jocelynjiang1@gmail.com>
2025-03-13 11:11:35 -0400

    fixed pause, stop/clear, new action+play bug

--- source/ButtonFunctions.cpp
+++ source/ButtonFunctions.cpp
@@ -6,28 +6,30 @@
 
 #define DRIVE_TIME 1350
 #define TURN_TIME 650
+#define ACTIONS_LIMIT 50 //hold up to 50 actions
 
 
 int num_actions = 0;
-char* actions = (char*)malloc(50*sizeof(char)); //hold up to 50 actions
+char* actions = (char*)malloc(ACTIONS_LIMIT*sizeof(char));
 int stop_flag=0;
 int pause_flag=-1;
 
 void updateQueue(int pin){
-    if(pin==MICROBIT_ID_IO_P13){
-        // uBit.serial.printf("adding a Forward\n");
-        actions[num_actions]='F';
+    if(num_actions<ACTIONS_LIMIT){
+        if(pin==MICROBIT_ID_IO_P13){
+            actions[num_actions]='F';
+        }
+        else if(pin==MICROBIT_ID_IO_P14){
+            actions[num_actions]='B'; //B for back/reverse
+        }
+        else if(pin==MICROBIT_ID_IO_P16){
+            actions[num_actions]='L';
+        }
+        else if(pin==MICROBIT_ID_IO_P15){
+            actions[num_actions]='R'; 
+        }
+        num_actions+=1;
     }
-    else if(pin==MICROBIT_ID_IO_P14){
-        actions[num_actions]='B'; //B for back/reverse
-    }
-    else if(pin==MICROBIT_ID_IO_P16){
-        actions[num_actions]='L';
-    }
-    else if(pin==MICROBIT_ID_IO_P15){
-        actions[num_actions]='R'; 
-    }
-    num_actions+=1;
 }
 
 static void addForward(MicroBitEvent){
@@ -60,7 +62,7 @@ static void printQueue(MicroBitEvent){
 
 static void playAll(MicroBitEvent){
     while(*actions!='\0'){
-        if(pause_flag==1){
+        if(pause_flag==1||stop_flag==1){
             break;
         }
         else if(*actions=='F'){
@@ -96,22 +98,26 @@ static void playAll(MicroBitEvent){
         uBit.sleep(1000);//take a break between actions
     }
     if(num_actions==0||stop_flag==1){ //exited loop because finished all actions, or terminated. Condition necessary so not resetting flag during an actual pause
+        uBit.serial.printf("Resetting flag\n");
         pause_flag=-1;
         stop_flag=0;
     }
-    //clear program outside of execution for loop because don't want for loop time to stall halting execution
-
-    // free(actions); //done executing all actions
+    uBit.serial.printf("Pause flag:%d\n",pause_flag);
+    uBit.serial.printf("Stop flag:%d\n",stop_flag);
 }
 
 static void stopHandler(MicroBitEvent){
-    if(pause_flag>-1){
+    if(pause_flag==0){
         stop_flag = 1; //means program stopped after running
     }
     for(int i=0;i<num_actions;i++){
         actions[i] = '\0'; //clear all actions in program
     }
     num_actions = 0; //clear program
+    pause_flag=-1;
+    uBit.serial.printf("Stop handler\n");
+    uBit.serial.printf("Pause flag:%d\n",pause_flag);
+    uBit.serial.printf("Stop flag:%d\n",stop_flag);
 }
 
 static void playHandler(MicroBitEvent){ //possibly call playHandler as a void() instead of another event --> ensure playHandler always before playAll, not concurrent
@@ -122,18 +128,6 @@ static void playHandler(MicroBitEvent){ //possibly call playHandler as a void() 
         if(num_actions>0){ //don't let messing with play button on empty program mess with flag
             pause_flag+=1; //set flag
         }
-    }
-}
-
-void listen_direction(){ //synchronous event handling
-    uBit.messageBus.listen(MICROBIT_ID_IO_P13, MICROBIT_BUTTON_EVT_CLICK, addForward);
-    uBit.messageBus.listen(MICROBIT_ID_IO_P14, MICROBIT_BUTTON_EVT_CLICK, addReverse);
-    uBit.messageBus.listen(MICROBIT_ID_IO_P16, MICROBIT_BUTTON_EVT_CLICK, addLeft);
-    uBit.messageBus.listen(MICROBIT_ID_IO_P15, MICROBIT_BUTTON_EVT_CLICK, addRight);
-    uBit.messageBus.listen(MICROBIT_ID_IO_P5, MICROBIT_BUTTON_EVT_CLICK, playAll); //504 error if try Stop button during Play
-    uBit.messageBus.listen(MICROBIT_ID_IO_P8, MICROBIT_BUTTON_EVT_CLICK, printQueue);
-    while(1){
-        uBit.sleep(1000);
     }
 }
 
