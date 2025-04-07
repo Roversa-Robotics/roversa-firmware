e2f7b172603c32c5fc70471d8e45c646e8528cc6
jocelynjiang1 <jocelynjiang1@gmail.com>
2025-04-07 11:39:20 -0400

    resolved ignoring first action after clear bug (wrong num_actions), clear working but not replacing memory from first char

--- source/ButtonFunctions.cpp
+++ source/ButtonFunctions.cpp
@@ -15,7 +15,12 @@ int stop_flag=0;
 int pause_flag=-1;
 
 void updateQueue(int pin){
+    uBit.serial.printf("updateQueue:\n");
+    uBit.serial.printf("num_actions:%d\n",num_actions);
+    uBit.serial.printf("pause_flag:%d\n",pause_flag);
     if(num_actions<ACTIONS_LIMIT && pause_flag==-1){ //can't add after start playing(0) or paused(1). If want to allow update during pause, change condition to !=0
+        uBit.serial.printf("Check actions[] after prev program\n");
+        uBit.serial.printf("actions[num_actions]: %s\n", actions[num_actions]);
         switch(pin) {
             case MICROBIT_ID_IO_P13:
                 actions[num_actions]='F';
@@ -95,11 +100,11 @@ static void playAll(MicroBitEvent){
                 break;
         }
         actions+=1; //iterate through commands
-        num_actions-=1; //num actions remaining decremented
         fiber_sleep(1000);
     }
     if(pause_flag==0){
         pause_flag=-1; // was in play (0), program just completed, reset flag for next program
+        num_actions = 0;
     }
 }
 
@@ -109,10 +114,6 @@ static void playHandler(MicroBitEvent){
     }
     else{
         if(num_actions>0){ //don't let play presses b/w empty queues mess with flag
-            if(pause_flag==-1){ //initial play
-                uBit.display.scrollAsync("Go!");
-                uBit.display.clear();
-            }
             pause_flag+=1; //set flag if queue has actions to do
         }
     }
@@ -120,9 +121,9 @@ static void playHandler(MicroBitEvent){
 
 static void stopHandler(MicroBitEvent){ //clear program, reset flags (whether or not program running)
     stop();
-    uBit.display.print(stop_sign);
+    uBit.serial.printf("stop, num_actions: %d",num_actions);
     for(int i=0;i<num_actions;i++){
-        actions[i] = '\0'; //no actions in playAll will match
+        actions[i] = 'c'; //no actions in playAll will match
     }
     num_actions = 0;
     pause_flag=-1;
@@ -135,6 +136,6 @@ void fiber_scheduler(){ //asynchronous event handling
     uBit.messageBus.listen(MICROBIT_ID_IO_P15, MICROBIT_BUTTON_EVT_CLICK, addRight);
     uBit.messageBus.listen(MICROBIT_ID_IO_P5, MICROBIT_BUTTON_EVT_CLICK, playAll);
     uBit.messageBus.listen(MICROBIT_ID_IO_P5, MICROBIT_BUTTON_EVT_CLICK, playHandler);
-    uBit.messageBus.listen(MICROBIT_ID_IO_P9, MICROBIT_BUTTON_EVT_CLICK, stopHandler, MESSAGE_BUS_LISTENER_IMMEDIATE);
+    uBit.messageBus.listen(MICROBIT_ID_IO_P9, MICROBIT_BUTTON_EVT_CLICK, stopHandler);
     uBit.messageBus.listen(MICROBIT_ID_IO_P8, MICROBIT_BUTTON_EVT_CLICK, printQueue);
 }
